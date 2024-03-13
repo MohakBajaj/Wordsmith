b54091e782e5edb97ffd43be1aeba67935aaeffc
Mohak Bajaj <bmohak87@gmail.com>
2024-03-13 10:06:14 +0530

    Add Header component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Wordsmith</h1>
+    <div className="bg-zinc-900 m-2 md:p-2 mon font-semibold md:overflow-y-hidden">
+      <Header />
     </div>
   );
 }
