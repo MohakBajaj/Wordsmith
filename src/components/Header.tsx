2c3db318b640e2cb4fd4082623d1a8eb9882a04b
Mohak Bajaj <bmohak87@gmail.com>
2024-03-13 10:04:54 +0530

    Add Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div>
+      <div className="flex flex-row">
+        <p className="text-4xl select-none">Text Utils 📝</p>
+      </div>
+    </div>
+  );
+}
